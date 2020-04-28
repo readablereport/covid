c4e1ef1d6be82ebe39d133ca8f1e113ac252a48d
Stephen Erke <stephen@gratifyhq.com>
2020-04-28 12:30:12 -0500

    Nate is saving files

--- components/FooterNav.vue
+++ components/FooterNav.vue
@@ -1,26 +1,46 @@
 <template>
 	<footer class="w-100 px-10 bg-martinique-500 relative z-10">
-		<div class="h-32 flex items-center justify-between">
-			<a href="https://readable.report/">
-				<p class="text-xs font-bold leading-loose text-melrose-300">powered by</p>
-				<img class="h-16" src="~assets/img/readable-report-icon-light.svg" alt="">
-			</a>
-			<p class="text-xs text-white font-bold">
-				DATA SOURCES: <a href="https://en.wikipedia.org/wiki/Template:2019%E2%80%9320_coronavirus_pandemic_data" target="_blank" class="text-melrose-300 underline hover:no-underline transition ease-in-out duration-700">Wikipedia</a> | <a href="https://github.com/CSSEGISandData/COVID-19" target="_blank" class="text-melrose-300 underline hover:no-underline transition ease-in-out duration-700">Johns Hopkins University</a> | <nuxt-link to="/about-this-data" class="text-melrose-300 underline hover:no-underline transition ease-in-out duration-700">About this data</nuxt-link>
-			</p>
+		<div class="h-32 flex flex-wrap pt-3 md:pt-0 md:flex-no-wrap md:items-center md:justify-between">
+			<div class="w-1/4">
+				<a href="https://readable.report/">
+					<span class="text-xs font-bold leading-loose text-melrose-100">powered by</span>
+					<img class="h-16" src="~assets/img/readable-report-icon-light.svg" alt="">
+				</a>
+			</div>
+			<div class="w-3/4 flex justify-end items-start">
+				<div class="text-xs text-white font-bold flex flex-wrap">
+					<div class="flex flex-wrap">
+						<div class="w-full mt-1 md:mt-0 text-right md:text-left">
+							<a class="text-xs font-bold leading-loose text-melrose-100">data sources</a>
+						</div>
+						<div class="w-full text-right md:text-left">
+							<a href="https://en.wikipedia.org/wiki/Template:2019%E2%80%9320_coronavirus_pandemic_data" target="_blank" class="transition ease-in-out duration-700 text-melrose-300 no-underline px-2 py-1 rounded-l bg-martinique-500 hover:bg-melrose-800">
+								Wikipedia
+							</a>
+							<a href="https://github.com/CSSEGISandData/COVID-19" target="_blank" class="transition ease-in-out duration-700 text-melrose-300 no-underline px-2 py-1 rounded-r bg-martinique-500 hover:bg-melrose-800">
+								Johns Hopkins CSSE
+							</a>
+						</div>
+					</div>
+					<div class="flex flex-wrap">
+						<div class="w-full mt-2 md:mt-0 text-right md:text-left">
+							<a class="text-xs font-bold leading-loose text-melrose-100">about</a>
+						</div>
+						<div class="w-full text-right md:text-left">
+							<nuxt-link to="/about-this-data" class="transition ease-in-out duration-700 text-melrose-300 no-underline px-2 py-1 rounded-l bg-martinique-500 hover:bg-melrose-800">
+								About this data
+							</nuxt-link>
+							<nuxt-link to="/privacy" class="transition ease-in-out duration-700 text-melrose-300 no-underline px-2 py-1 rounded-r bg-martinique-500 hover:bg-melrose-800">
+								Privacy Policy
+							</nuxt-link>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</footer>
 </template>
-
 <script>
 </script>
-
 <style scoped lang="css">
-footer {
-	background-image: url('~assets/img/footer-bg.svg');
-	background-repeat: repeat-x;
-	box-shadow: inset 0 30px 60px rgba(30, 30, 50, 0.5);
-	background-position: bottom;
-	background-attachment: fixed;
-}
 </style>
--- pages/about-this-data.vue
+++ pages/about-this-data.vue
@@ -1,7 +1,7 @@
 <template>
 	<main class="crr--h-screen-minus-header flex">
 		<section class="w-full md:w-2/3 mx-auto px-8 lg:px-16">
-			<h1 class="text-4xl md:text-5xl font-extrabold text-martinique-500 mt-8 md:mt-10">
+			<h1 class="text-4xl font-extrabold text-martinique-500 mt-8 md:mt-10">
 				Local COVID-19 statistics data
 			</h1>
 			<h2 class="text-2xl font-bold text-melrose-700">
@@ -14,16 +14,22 @@
 				This data changes rapidly and might not reflect all cases as the reporting time and frequency vary.
 			</p>
 			<h3 class="text-xl font-bold text-gray-700 mt-6">
-				It comes from Wikipedia and Johns Hopkins
+				It comes from Wikipedia and Johns Hopkins CSSE
 			</h3>
 			<p class="text-base text-gray-700">
-				The data comes from <a href="https://en.wikipedia.org/wiki/Template:2019%E2%80%9320_coronavirus_pandemic_data" target="_blank" class="underline text-melrose-600 hover:no-underline transition ease-in-out duration-700">Wikipedia</a> and <a href="https://github.com/CSSEGISandData/COVID-19" target="_blank" class="underline text-melrose-600 hover:no-underline transition ease-in-out duration-700">Johns Hopkins University</a>.
+				The data comes from <a href="https://en.wikipedia.org/wiki/Template:2019%E2%80%9320_coronavirus_pandemic_data" target="_blank" class="underline text-melrose-600 hover:no-underline transition ease-in-out duration-700">Wikipedia</a> and <a href="https://github.com/CSSEGISandData/COVID-19" target="_blank" class="underline text-melrose-600 hover:no-underline">Johns Hopkins CSSE</a>.
 			</p>
 			<h3 class="text-xl font-bold text-gray-700 mt-6">
 				Why do I get different data from different sources?
 			</h3>
 			<p class="text-base text-gray-700">
 				Reporting varies as there are numerous sources of coronavirus data. Each source may update, gather, or aggregate in a different way.
+			</p>
+			<h3 class="text-xl font-bold text-gray-700 mt-6">
+				How can I help?
+			</h3>
+			<p class="text-base text-gray-700">
+				This is an open source project and we would love for you to contribute. You can do so via <a href="https://github.com/readablereport/covid" target="_blank" class="underline text-melrose-600 hover:no-underline">GitHub</a>.
 			</p>
 		</section>
 	</main>
